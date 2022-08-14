d5fbab9ad416b0255252b13b6f4df2595e189fb7
Fagboye <>
2022-08-14 21:15:42 +0000

    I have added the code for rice and amala

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import random 
+# I define the base constituents of the meals
+Base = ['rice', 'spaghetti', 'noodles', 'Amala']
+primary_meal = random.choice(Base)
 
-primaryMeal = ['rice', 'spaghetti', 'noodles', 'Amala']
+
+if primary_meal == 'rice':
+    secondary = ['white', 'fried', 'jollof']
+    random_secondary = random.choice(secondary)
+    print(f'{random_secondary} {primary_meal}')
+
+if primary_meal == 'Amala':
+  soup = ['egusi', 'efo']
+  r_soup = random.choice(soup)
+  print(f'{primary_meal} and {r_soup}')
